b50bc411adb3ad14e99c0b87941fb72c91b2b9f9
Hector Alfaro <hectorsector@github.com>
2016-09-07 12:53:04 -0400

    mod server

--- bin/server.rb
+++
@@ -1,4 +0,0 @@
-post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "I got some JSON: #{push.inspect}"
-end
--- server.rb
+++ server.rb
@@ -1,3 +1,6 @@
+require 'sinatra'
+require 'json'
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
