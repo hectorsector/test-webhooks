7cef6c21b9553dda0471cabcba8a0b66aed221a3
Hector Alfaro <hectorsector@github.com>
2016-09-08 11:17:51 -0400

    try updating server

--- server.rb
+++ server.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
 require 'json'
 
+jsontest = "none"
+
 post '/payload' do
   push = JSON.parse(request.body.read)
   puts "I got some JSON: #{push.inspect}"
+  jsontest = "got some!"
+end
+
+get '/payload' do
+  jsontest
 end
